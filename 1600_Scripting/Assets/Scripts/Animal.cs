ffd412e0b9f1425734864014590420137ef52b16
Unknown <jacobaflint96@gmail.com>
2018-05-16 11:19:40 -0600

    Added Data Types

---
+++ 1600_Scripting/Assets/Scripts/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+    public float Speed;
+    public int PowerLevel;
+    public string PlayerName;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Speed, Speed, Speed);
+        transform.Translate(Speed, 0, 0);
+    }
+}
