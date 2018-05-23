d62205fcef6e260558d859aa268d59d4c00368ae
Unknown <jacobaflint96@gmail.com>
2018-05-23 11:24:17 -0600

    Added Power Ups

---
+++ 1600_Scripting/Assets/Scripts/PowerUpBehavior.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpBehavior : MonoBehaviour {
+    public PowerUpBase PowerUp;
+    private void OnMouseDown()
+    {
+        Debug.Log(PowerUp.PowerLevel);
+    }
+}
