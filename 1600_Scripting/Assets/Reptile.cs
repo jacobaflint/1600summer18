4c885784c7dbaac022d7b6d80ceb1e0b08970f30
Unknown <jacobaflint96@gmail.com>
2018-05-25 15:06:12 -0600

    Added Reptile Script

---
+++ 1600_Scripting/Assets/Reptile.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class Reptile : Animal {
+
+    public int ScaleCount = 250;
+    public Color ScaleColor;
+}
