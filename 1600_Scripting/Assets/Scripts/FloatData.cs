8d1c480ef3f5b9bf89b216f8e055599e6a2362c2
Unknown <jacobaflint96@gmail.com>
2018-06-04 11:38:20 -0600

    Float Data Work & Clean Up

---
+++ 1600_Scripting/Assets/Scripts/FloatData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject {
+
+    [Range(0,1)] public float Value;
+
+}
