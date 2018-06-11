1159d5956f80642754f705dbb66715f0e1df0e9b
Unknown <jacobaflint96@gmail.com>
2018-06-11 11:26:44 -0600

    Character Controller

---
+++ 1600_Scripting/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+	
+	private CharacterController controller;
+
+	private Vector3 newPosition;
+	public float Speed = 10.0f;
+	public float Gravity = 9.81f;
+	public float JumpSpeed = 10.0f;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			newPosition.y = JumpSpeed;
+		}
+		
+		newPosition.y -= Gravity;
+		newPosition.x = Speed*Input.GetAxis("Horizontal");
+		controller.Move(newPosition * Time.deltaTime);
+	}
+}
