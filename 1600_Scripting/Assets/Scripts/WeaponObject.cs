b6c1a4f8aa97a2ab19308d8825f65a921c5b08f0
Unknown <jacobaflint96@gmail.com>
2018-06-26 23:12:08 -0600

    Added Enums

---
+++ 1600_Scripting/Assets/Scripts/WeaponObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class WeaponObject : ScriptableObject
+{
+
+    public float DamageLevel = 10;
+
+    public float DoDamage()
+    {
+        return DamageLevel;
+    }
+    
+}
