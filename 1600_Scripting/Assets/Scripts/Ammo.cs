c7023e9b5d9a9f51a39db882618e1b0f38f386f7
Unknown <jacobaflint96@gmail.com>
2018-06-27 11:23:14 -0600

    Co-routines/For Loops/Ammo

---
+++ 1600_Scripting/Assets/Scripts/Ammo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+	private Rigidbody projectile;
+	public FloatData ProjectileSpeed;
+	public FloatData ProjectileUpgrade;
+	
+
+	// Use this for initialization
+	void OnEnable()
+	{
+		projectile = GetComponent<Rigidbody>();
+		projectile.AddForce(0, 0, ProjectileSpeed.Value);
+		Invoke("Deactivate", 2);
+
+	}
+
+	private void Deactivate()
+	{
+		gameObject.SetActive(false);
+	}
+		
+	
+	// Update is called once per frame
+	private void OnTriggerEnter(Collider other)
+	{
+		ProjectileSpeed = ProjectileUpgrade;
+		projectile.AddForce(0, 0, ProjectileSpeed.Value);
+	}
+}
+
---
+++ 1600_Scripting/Assets/Scripts/FireAmmo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireAmmo : MonoBehaviour
+{
+
+    public List<GameObject> Ammo;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            for (int i = 0; i < Ammo.Count; i++)
+            {
+                Ammo[i].SetActive(true);
+            }
+        }
+    }
+}
