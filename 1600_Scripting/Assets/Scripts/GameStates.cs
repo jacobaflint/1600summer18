dc6213c8722f7017d63708cfff0dde2dad867838
Unknown <jacobaflint96@gmail.com>
2018-07-25 11:29:32 -0600

    Review

---
+++ 1600_Scripting/Assets/Scripts/GameStates.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStates : MonoBehaviour
+{
+    public GameStateBase Starting;
+    public GameStateBase Playing;
+    public GameStateBase Ending;
+    
+    public enum States
+    {
+        Starting,
+        Playing,
+        Ending
+    }
+
+    private void Update()
+    {
+        ChangeState();
+    }
+
+    private States CurrentState;
+
+    public void ChangeState()
+    {
+        switch (CurrentState)
+        {
+            case States.Starting:
+                Starting.Run();
+                break;
+            case States.Playing:
+                Playing.Run();
+                break;
+            case States.Ending:
+                Ending.Run();
+                break;
+        }
+    }
+}
---
+++ 1600_Scripting/Assets/Scripts/GameStates/GameStateBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateBase : ScriptableObject, IGameState 
+{
+        public virtual void Run()
+        {
+            Debug.Log("Running");
+        }
+}
+
+    public interface IGameState
+    {
+        void Run();
+    }
